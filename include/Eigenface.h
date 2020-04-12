8c28ef5bbc88134bc961f366e3af27e4787ef5f6
kylebrain <kbrainard@nevada.unr.edu>
2020-04-11 17:49:58 -0700

    Add const qualifiers to Eigenface functions and GetAverageImage and GetEigenfaceImages

--- include/Eigenface.h
+++ include/Eigenface.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include "Eigen/Dense"
+#include "Image.h"
 
 using namespace Eigen;
 
@@ -31,11 +32,17 @@ class Eigenface
 
         // testingImages - (N^2 x K)-matrix of testing images
         // Returns the image ID of the closest topMatches matches for each column of testingImages
-        std::vector<std::vector<int>> GetClosestMatches(MatrixXf testingImages, int topMatches);
+        std::vector<std::vector<int>> GetClosestMatches(MatrixXf testingImages, int topMatches) const;
 
         // testingImages - (N^2 x K)-matrix of testing images
         // Returns the error/distance between the reconstructed and normalized image for each column of testingImages
-        std::vector<float> GetDetectionError(MatrixXf testingImages);
+        std::vector<float> GetDetectionError(MatrixXf testingImages) const;
+
+        // Returns the averageFace in image format
+        Image<GreyScale> GetAverageImage() const;
+
+        // Returns the eigenfaces/vectors in image format on range [start, end)
+        std::vector<Image<GreyScale>> GetEigenfaceImages(int start, int end) const;
     
 
     private:
@@ -43,7 +50,7 @@ class Eigenface
         void GetTrainingData(std::string trainingDirectory);
 
         // Returns the (N^2)-vector of the average of the colums of (N^2 x M)-matrix images
-        VectorXf GetAverageFace(MatrixXf images);
+        VectorXf GetAverageFace(MatrixXf images) const;
 
         // normalizedImages - (N^2 x M)-matrix aka A in which the columns are the normalized images
         // Sets the eigenvalues and eigenfaces members
@@ -52,32 +59,32 @@ class Eigenface
 
         // images - (N^2 x M, K, 1)-matrix
         // Returns an (M x M, K, 1)-matrix in which the columns are the eigenspace representation of the columns of images
-        MatrixXf ComputeEigenSpaceValues(MatrixXf images);
+        MatrixXf ComputeEigenSpaceValues(MatrixXf images) const;
 
         // images - (N^2 x M, K, 1)-matrix
         // Subtracts the average face from each column of images and returns the resulting value
-        MatrixXf NormalizeImages(MatrixXf images);
+        MatrixXf NormalizeImages(MatrixXf images) const;
 
         // eigenspaceValues - (M x M, K, 1)-matrix
         // eigenCount - number of eigenvectors used in the reconstruction
             // Must be less than or equal to M
         // Reconstructs the columns of eigenspaceValues using the first eigenCount eigenfaces
-        MatrixXf ReconstructImages(MatrixXf eigenspaceValues, int eigenCount);
+        MatrixXf ReconstructImages(MatrixXf eigenspaceValues, int eigenCount) const;
 
         // Outputs the averageFace, eigenFaces, and the eigenspaceTrainingValues to a file
-        void OutputTrainingData(std::string outputFileName);
+        void OutputTrainingData(std::string outputFileName) const;
 
         // Returns the MahalanobisDistance between the eigenspace representations of the two images
         // eigenspaceImage - (M)-vector
         // eigenCount - number of eigenvectors considered in the calculation
             // Must be less than or equal to M
-        float MahalanobisDistance(VectorXf eigenspaceImage1, VectorXf eigenspaceImage2, int eigenCount);
+        float MahalanobisDistance(VectorXf eigenspaceImage1, VectorXf eigenspaceImage2, int eigenCount) const;
 
         // Returns the EuclideanDistance between the eigenspace representations of the two images
         // eigenspaceImage - (M)-vector
         // eigenCount - number of eigenvectors considered in the calculation
             // Must be less than or equal to M
-        float EuclideanDistance(VectorXf eigenspaceImage1, VectorXf eigenspaceImage2, int eigenCount);
+        float EuclideanDistance(VectorXf eigenspaceImage1, VectorXf eigenspaceImage2, int eigenCount) const;
 };
 
 
